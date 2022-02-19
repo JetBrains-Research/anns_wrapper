f3996c2b113bb81890f83aa0ca28414b142648e0
elijah <kelim.mail@gmail.com>
2022-02-19 15:55:25 +0300

    Flann WIP;

--- wrappers/flann.py
+++ wrappers/flann.py
@@ -1,6 +1,7 @@
 from typing import Any, List
 
 import numpy
+import pyflann
 from numpy.random import *
 from pyflann import *
 
@@ -8,7 +9,7 @@ from interface import NNSWrapper
 from utility.metrics import Metrics
 
 
-class FlannAnnoyWrapper(NNSWrapper):
+class FlannWrapper(NNSWrapper):
     """
     Bruteforce realisation.
     """
@@ -17,6 +18,7 @@ class FlannAnnoyWrapper(NNSWrapper):
     max_item_num = 0
 
     def __init__(self, dimensions: int, distance: str):
+        pyflann.index.set_distance_type(distance)
         self.index = FLANN()
         self.initial_index_vectors = []
         self.pool = []
@@ -24,6 +26,7 @@ class FlannAnnoyWrapper(NNSWrapper):
         self.distance_name = distance
         self.distance = Metrics.get_metric(self.distance_name)
         self.deleted_ids = []
+        self.params = None
 
     def add_vector_to_index(self, vector: numpy.ndarray):
         self.initial_index_vectors.append(vector)
@@ -50,33 +53,24 @@ class FlannAnnoyWrapper(NNSWrapper):
             self.deleted_ids.append(d_vector)
 
     def build_index(self, target_precision=0.9, log_level="info"):
-        self.index.build_index(self.initial_index_vectors, target_precision=target_precision, log_level=log_level)
+        self.params = self.index.build_index(numpy.array(self.initial_index_vectors), target_precision=target_precision,
+                                             log_level=log_level)
+        self.initial_index_vectors = []
         self.is_built = True
 
     def rebuild_index(self, trees: int = 10):
-        index = FLANN()
-
-        skipped = 0
-        for i in range(self.index.get_n_items()):
-            if i not in self.deleted_ids:
-                index.add_item(i - skipped, self.index.get_item_vector(i))
-            else:
-                skipped += 1
-        for i, (id, vector) in enumerate(self.pool):
-            index.add_item(self.index.get_n_items() - skipped + i, vector)
-
-        index.build(trees)
-
-        self.index = index
+        self.initial_index_vectors = [x for x in self.index.__dict__['_FLANN__curindex_data'] if
+                                      x not in self.deleted_ids] + self.pool
+        self.build_index()
         self.pool = []
         self.deleted_ids = []
 
-        self.is_built = True
-
     def search(self, vector: numpy.ndarray, neighbors: int, include_distances: bool = False):
-        pool_distances = [(x[0], self.distance(vector, x[1])) for x in self.pool]
-        index_distances = list(zip(*self.index.get_nns_by_vector(vector, neighbors * 2, include_distances=True)))
-        distances = [x for x in pool_distances + index_distances if x[0] not in self.deleted_ids]
+        query = self.index.nn_index(numpy.array([vector]), neighbors * 2, checks=self.params["checks"])
+        print(query)
+        pool_distances = [(x, self.distance(vector, x)) for x in self.pool]
+        index_distances = list(zip(*query))
+        distances = [x for x in pool_distances + index_distances if x not in self.deleted_ids]
         res = sorted(distances, key=lambda x: x[1])[:neighbors]
         if include_distances:
             return res
@@ -85,9 +79,18 @@ class FlannAnnoyWrapper(NNSWrapper):
 
 
 if __name__ == '__main__':
-    dataset = rand(10000, 128)
+    dataset = rand(100, 128)
     testset = rand(1000, 128)
     flann = FLANN()
-    params = flann.build_index(dataset, target_precision=0.9, log_level="info");
-    result, dists = flann.nn_index(testset, 5, checks=params["checks"]);
-    print(result)
+    # params = flann.build_index(dataset, target_precision=0.9, log_level="info")
+    w = FlannWrapper(128, "euclidean")
+    for i in dataset:
+        w.add_vector(i)
+    w.build_index()
+
+    w.rebuild_index()
+    print(w.search(dataset[0], 3))
+
+    # print(flann.__dict__['_FLANN__curindex_data'])
+    # result, dists = flann.nn_index(testset, 5, checks=params["checks"]);
+    # print(result)
